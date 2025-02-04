4110d8545a841e7e37fe91c7f5423fede490e397
Rhitajyoti Mandal <rhitajyotim411@gmail.com>
2025-02-04 22:06:57 +0530

    student entity created

---
+++ src/main/java/com/marionette/student_management/model/Student.java
@@ -0,0 +1,22 @@
+package com.marionette.student_management.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "phone", nullable = false, unique = true)
+    private String phone;
+}
