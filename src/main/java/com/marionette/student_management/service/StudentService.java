1ce615c5274f10b873b59e51cc9892b84c4f38c1
Rhitajyoti Mandal <rhitajyotim411@gmail.com>
2025-02-04 22:24:19 +0530

    student service added

---
+++ src/main/java/com/marionette/student_management/service/StudentService.java
@@ -0,0 +1,31 @@
+package com.marionette.student_management.service;
+
+import com.marionette.student_management.model.Student;
+import com.marionette.student_management.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    public void deleteStudentById(Long id) {
+        studentRepository.deleteById(id);
+    }
+}
