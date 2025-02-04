d6313d2a37fbb29250611e7f31c475cf59c3bff6
Rhitajyoti Mandal <rhitajyotim411@gmail.com>
2025-02-04 22:35:25 +0530

    student controller added

---
+++ src/main/java/com/marionette/student_management/controller/StudentController.java
@@ -0,0 +1,35 @@
+package com.marionette.student_management.controller;
+
+import com.marionette.student_management.model.Student;
+import com.marionette.student_management.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+    @Autowired
+    StudentService studentService;
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @PostMapping
+    public Student saveStudent(@RequestBody Student student) {
+        return studentService.saveStudent(student);
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable Long id) {
+        return studentService.getStudentById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudentById(@PathVariable Long id) {
+        studentService.deleteStudentById(id);
+    }
+}
